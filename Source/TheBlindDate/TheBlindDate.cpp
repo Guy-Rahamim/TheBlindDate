97509b7f6318d931272182987630982ff22141b3
Guy rahamim <guy.danino5@gmail.com>
2020-09-01 14:58:23 +0300

    Initial commit

---
+++ Source/TheBlindDate/TheBlindDate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheBlindDate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheBlindDate, "TheBlindDate" );
---
+++ Source/TheBlindDate/TheBlindDateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheBlindDateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEBLINDDATE_API ATheBlindDateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
